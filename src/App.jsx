e673349b2947f35a40be4f4a4fa0f24a3ac7ecf4
Jay Patel <jay.patel1@gameskraft.com>
2024-06-19 22:01:18 +0530

    init redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useEffect, useState } from 'react';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
-import { useAppContext } from './context/useAppContext';
-
 function App() {
-  const { commonActions } = useAppContext();
-
-  useEffect(()=>{
-    commonActions.initApp();
-  },[]);
-
   return (
-    <div className="bg-gray-200 w-screen h-screen flex flex-col items-center justify-start overflow-x-hidden overflow-y-scroll">
-      <div className='max-w-[300px] md:max-w-[800px] w-full p-2 mt-4'>
-        <TodoForm/>
-        <TodoList />
-      </div>
+    <div>
+      test
     </div>
   )
 }
